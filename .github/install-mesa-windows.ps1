2c93a261737e1e799d1279066dd1e5f96dc2d4b6
Daniel Henk <henk.daniel98@gmail.com>
2021-11-06 22:08:42 +0100

    Add hack for opengl

---
+++ .github/install-mesa-windows.ps1
@@ -0,0 +1,21 @@
+function grantRights($file) {
+	takeown /f $file
+	icacls $file /grant "${env:ComputerName}\${env:UserName}:F"
+}
+
+function InstallMesaOpenGL ($arch, $basedir) {
+	$source_path = "./download/build/$arch/opengl32.dll"
+	$filepath = $basedir + "opengl32.dll"
+	if (Test-Path $filepath) {
+		grantRights $filepath
+		Rename-Item -Path $filepath -NewName opengl32.dll_old
+	}
+	cp $source_path $filepath
+}
+fu
+
+wget https://github.com/jvbsl/MesaBinary/releases/download/19/opengl_win_dll.zip -OutFile download.zip
+Expand-Archive -Force ./download.zip
+
+InstallMesaOpenGL "x86" "$env:WINDIR\SysWOW64\"
+InstallMesaOpenGL "x86_64" "$env:WINDIR\system32\"
