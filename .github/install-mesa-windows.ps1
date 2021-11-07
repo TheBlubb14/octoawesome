62f1c8d1b6ed652df38cb962515ec9301f53ab20
Daniel Henk <henk.daniel98@gmail.com>
2021-11-07 17:27:26 +0100

    Removed docs

--- .github/install-mesa-windows.ps1
+++ .github/install-mesa-windows.ps1
@@ -14,7 +14,10 @@ function InstallMesaOpenGL ($arch, $basedir) {
 }
 
 Invoke-WebRequest https://github.com/jvbsl/MesaBinary/releases/download/21/opengl_win_dll.zip -OutFile download.zip
+Invoke-WebRequest https://github.com/gkv311/wglinfo/releases/download/19_05/wglinfo64.exe -OutFile wglinfo.exe
 Expand-Archive -Force ./download.zip
 
 InstallMesaOpenGL "x86" "$env:WINDIR\SysWOW64\"
 InstallMesaOpenGL "x86_64" "$env:WINDIR\system32\"
+
+./wglinfo.exe
