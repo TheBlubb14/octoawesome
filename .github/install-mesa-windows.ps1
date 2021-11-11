b7642213ed30b7236875ad49fe9a5387adc9a00b
Daniel Henk <henk.daniel98@gmail.com>
2021-11-11 22:35:56 +0100

    Update install-mesa-windows.ps1

--- .github/install-mesa-windows.ps1
+++ .github/install-mesa-windows.ps1
@@ -1,26 +1,20 @@
-Set-PSDebug -Trace 1
 function grantRights($file) {
 	takeown /f $file
 	icacls $file /grant "${env:ComputerName}\${env:UserName}:F"
 }
 
-function InstallMesaOpenGL ($arch, $basedir) {
-	$source_path = "./download/$arch/opengl32.dll"
+function InstallMesaOpenGL ($basedir) {
 	$filepath = $basedir + "opengl32.dll"
 	if (Test-Path $filepath) {
 		grantRights $filepath
 		Rename-Item -Path $filepath -NewName opengl32.dll_old
 	}
-	Copy-Item $source_path $filepath
+	$source_path = "./download/*"
+	Copy-Item -Path $source_path -Destination $basedir
 }
 
-Invoke-WebRequest https://github.com/jvbsl/MesaBinary/releases/download/21/opengl_win_dll.zip -OutFile download.zip
-Invoke-WebRequest https://github.com/gkv311/wglinfo/releases/download/19_05/wglinfo64.exe -OutFile wglinfo64.exe
+Invoke-WebRequest https://github.com/jvbsl/MesaBinary/releases/download/21_shared/mesa_x64-windows_dll.zip -OutFile download.zip
 Expand-Archive -Force ./download.zip
 
-InstallMesaOpenGL "x86" "$env:WINDIR\SysWOW64\"
-InstallMesaOpenGL "x86_64" "$env:WINDIR\system32\"
+InstallMesaOpenGL "$env:WINDIR\system32\"
 Write-Host "Done"
-$global:LASTEXITCODE = 0
-wglinfo64.exe 
-$global:LASTEXITCODE = 0
